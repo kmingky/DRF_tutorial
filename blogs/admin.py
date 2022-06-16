a403f4965b7ba1f91e54c1716d413e0811f134e7
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-06-17 01:48:37 +0900

    Add user,category,article in admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from blogs.models import Category as CategoryModel
+from blogs.models import Article as ArticleModel
+
 # Register your models here.
+admin.site.register(CategoryModel)
+admin.site.register(ArticleModel)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from users.models import User as UserModel
+
 # Register your models here.
+admin.site.register(UserModel)
