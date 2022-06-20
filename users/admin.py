eaf7f4006bcc19bce3e70677a27e8c7d3d9568e2
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-06-20 22:46:54 +0900

    Add user admin

--- users/admin.py
+++ users/admin.py
@@ -4,8 +4,40 @@ from users.models import User as UserModel
 from users.models import UserProfile as UserProfileModel
 from users.models import Hobby as HobbyModel
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+# admin 페이지에 user admin을 등록하고, userprofile 테이블을 user admin 페이지에서 같이 보고 설정 할 수 있도록 해주세요
+class UserProfileInline(admin.StackedInline):
+    model = UserProfileModel
+    filter_horizontal = ["hobby"]
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'fullname')
+    list_display_links = ('username', )
+    list_filter = ('email', )
+    search_fields = ('username', 'email')
+
+    fieldsets = (
+         ("info", {'fields': ('username', 'password', 'email', 'fullname', 'join_date')}),
+        ('permissions', {'fields': ('is_admin', 'is_active', )})
+
+    )
+
+    filter_horizontal = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ("username", "join_date")
+        else:
+            return ("join_date")
+
+    inlines = (
+            UserProfileInline,
+        )
+
 # Register your models here.
-admin.site.register(UserModel)
+admin.site.register(UserModel, UserAdmin)
 admin.site.register(UserProfileModel)
 admin.site.register(HobbyModel)
 
